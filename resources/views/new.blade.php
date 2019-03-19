53466bd6e72c1e916b759e257d953803ab8c6b8d
Shraddha Thakkar <st746@njit.edu>
2019-03-19 16:41:21 -0400

    final deployment.

---
+++ resources/views/new.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About Me</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #495057;
+            color: #202326;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 800;
+            height: 500vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 60vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position:-ms-device-fixed;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 14px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 25px;
+        }
+
+        .links > a {
+            color: #e2f0fb;
+            padding: 0 25px;
+            font-size: 15px;
+            font-weight: 800;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="title m-b-md" >
+            Hello Professor,
+
+            This is Shraddha Thakkar. This is my second semster in NJIT. I am business and information system major. Currently,
+            I am working as lead teller at Wells Fargo bank. I am looking forward to finish my major and step into real world with knowledge within my field.
+            Looking forward to learn lot from you from this class.
+        </div>
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home Page</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #495057;
+                color: #202326;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 800;
+                height: 500vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 30vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 28px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #e2f0fb;
+                padding: 0 25px;
+                font-size: 15px;
+                font-weight: 800;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md" >
+                    My First Laravel Web Site
+                </div>
+
+                <div class="links">
+                    <a href="https://bootstrapheroku303.herokuapp.com">My First BootStrap Site</a>
+                    <a href="https://csvtohtml303.herokuapp.com">CSV To HTML Data Table</a>
+                    <a href="https://github.com"> My GitHub Account </a>
+                    <a href="http://localhost:8000/new">About me</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
